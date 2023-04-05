b10a1f2fa7d53cb1f072becf9895b76313df153a
Raahulwe2code <rahul.verma.we2code@gmail.com>
2023-04-05 10:36:46 +0530

    first_time_commit

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -197,7 +197,6 @@ export async function search_product(req, res) {
       var offset = 0;
     }
     console.log("limit--" + limit);
-
     console.log("offset--" + offset);
     connection.query(
       "SELECT * FROM `product` where 1 order BY name Desc LIMIT " +
